7c377213b71d00e0c9e907888c54f5897fcf89d2
Michael Seelisch <info@michaelseelisch.de>
2015-09-07 22:11:49 +0200

    no message

---
+++ NativeScript - Calculator/Project1/platforms/ios/build/Project1.build/Debug-iphoneos/Project1.build/Objects-normal/armv7/main.d
@@ -0,0 +1,6 @@
+dependencies: \
+  /Applications/MAMP/htdocs/Private/Mobile\ Projects/NativeScript\ -\ Calculator/Project1/platforms/ios/Project1/main.m \
+  /Applications/MAMP/htdocs/Private/Mobile\ Projects/NativeScript\ -\ Calculator/Project1/platforms/ios/NativeScript/include/NativeScript.h \
+  /Applications/MAMP/htdocs/Private/Mobile\ Projects/NativeScript\ -\ Calculator/Project1/platforms/ios/NativeScript/include/TNSRuntime.h \
+  /Applications/MAMP/htdocs/Private/Mobile\ Projects/NativeScript\ -\ Calculator/Project1/platforms/ios/NativeScript/include/TNSRuntime+Inspector.h \
+  /Applications/MAMP/htdocs/Private/Mobile\ Projects/NativeScript\ -\ Calculator/Project1/platforms/ios/Project1/TNSDebugging.h
