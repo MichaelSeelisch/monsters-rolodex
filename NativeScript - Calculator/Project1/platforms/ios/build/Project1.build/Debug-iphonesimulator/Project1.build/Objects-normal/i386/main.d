d6415b082d601628ab3e19e4b098208d98902b2c
Michael Seelisch <seelisch@kolle-rebbe.de>
2015-09-23 16:54:36 +0200

    Moved project "NativeScript - Calculator" from Web to Mobile

--- NativeScript - Calculator/Project1/platforms/ios/build/Project1.build/Debug-iphonesimulator/Project1.build/Objects-normal/i386/main.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Applications/MAMP/htdocs/Private/Mobile\ Projects/NativeScript\ -\ Calculator/Project1/platforms/ios/Project1/main.m \
-  /Applications/MAMP/htdocs/Private/Mobile\ Projects/NativeScript\ -\ Calculator/Project1/platforms/ios/NativeScript/include/NativeScript.h \
-  /Applications/MAMP/htdocs/Private/Mobile\ Projects/NativeScript\ -\ Calculator/Project1/platforms/ios/NativeScript/include/TNSRuntime.h \
-  /Applications/MAMP/htdocs/Private/Mobile\ Projects/NativeScript\ -\ Calculator/Project1/platforms/ios/NativeScript/include/TNSRuntime+Inspector.h \
-  /Applications/MAMP/htdocs/Private/Mobile\ Projects/NativeScript\ -\ Calculator/Project1/platforms/ios/Project1/TNSDebugging.h
