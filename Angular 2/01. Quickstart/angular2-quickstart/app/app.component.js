6a9fccc7e3e23276a185a8cd470bac6a13a9d36c
Michael Seelisch <seelisch@kolle-rebbe.de>
2016-01-12 17:26:03 +0100

    Update project "Node.js in Action"

--- Angular 2/01. Quickstart/angular2-quickstart/app/app.component.js
+++ Angular 2/01. Quickstart/angular2-quickstart/app/app.component.js
@@ -2,9 +2,16 @@
   app.AppComponent = ng.core
   	.Component({
       selector: 'my-app',
-      template: '<h1>My First Angular 2 App</h1>'
+      template: '<h1 class="headline">My very First Angular 2 App</h1>',
+      styles: [
+          '.headline { color: red; }'
+      ]
     })
     .Class({
-      constructor: function() {}
+      constructor: function() {},
+      
+      ngOnInit: function() {
+          console.log('Angualr 2 App initialized!');
+      }
     });
 })(window.app || (window.app = {}));
