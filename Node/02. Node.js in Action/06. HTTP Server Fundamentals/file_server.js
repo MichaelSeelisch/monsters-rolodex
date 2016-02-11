4fbf85ed500b4c9f086f5a33abb8a9b316b22264
Michael Seelisch <seelisch@kolle-rebbe.de>
2016-02-11 15:32:25 +0100

    Update project "Node.js in Action"

--- Node/02. Node.js in Action/06. HTTP Server Fundamentals/file_server.js
+++ Node/02. Node.js in Action/06. HTTP Server Fundamentals/file_server.js
@@ -4,16 +4,33 @@ var http    = require('http'),
     fs      = require('fs'),
     
     root    = __dirname;
-    
+
+/* Static file server long version */
+/*    
+    var server = http.createServer(function(req, res) {
+        var url     = parse(req.url);
+        var path    = join(root, url.pathname);
+        var stream  = fs.createReadStream(path);
+        stream.on('data', function(chunk) {
+            res.write(chunk);
+        });
+        stream.on('end', function() {
+            res.end();
+        });
+    });
+*/
+
 var server = http.createServer(function(req, res) {
     var url     = parse(req.url);
     var path    = join(root, url.pathname);
     var stream  = fs.createReadStream(path);
-    stream.on('data', function(chunk) {
-        res.write(chunk);
-    });
-    stream.on('end', function() {
-        res.end();
-    });
+    stream.pipe(res);  
 });
+
 server.listen(3000);
+
+/*
+  1. Terminal-Fenster 1 starten, dann: 'node file_server.js'
+  2. Terminal-Fenster 2 starten, dann:
+      curl http://localhost:3000/file_server.js -i (das i ist zum Anzeigen des Headers)
+*/
