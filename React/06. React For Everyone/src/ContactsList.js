30703fb5b471ca0faaaea101ce1c3541e2a6522c
Michael Seelisch <info@michaelseelisch.de>
2016-02-25 20:37:43 +0100

    Update project "React For Everyone"

--- React/06. React For Everyone/src/ContactsList.js
+++ React/06. React For Everyone/src/ContactsList.js
@@ -2,15 +2,40 @@ import React from 'react';
 import Contact from './Contact';
 
 class ContactsList extends React.Component {
-    render() {
-        return (
-            <ul>
-                {this.props.contacts.map((contact) => {
-                    return <Contact contact={contact} key={contact.id} />
-                })}
-            </ul>
-        )
-    }
+  constructor() {
+    super();
+    this.state = {
+      search: ''
+    };
+  }
+
+  updateSearch(event) {
+    this.setState({search: event.target.value.substr(0, 20)});
+  }
+
+  render() {
+    let filteredContacts = this.props.contacts.filter(
+      (contact) => {
+        return contact.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
+      }
+    );
+
+    return (
+      <div>
+
+        <input type='text'
+          value={this.state.search}
+          onChange={this.updateSearch.bind(this)} />
+        
+        <ul>
+          {filteredContacts.map((contact) => {
+            return <Contact contact={contact} key={contact.id} />
+          })}
+        </ul>
+
+      </div>
+    )
+  }
 }
 
 export default ContactsList;
