c7118925fe314d4170b31b3a61edd6a84f6d2bfc
Michael Seelisch <seelisch@kolle-rebbe.de>
2016-03-24 17:38:02 +0100

    Added project "Meteor & React"

---
+++ Meteor/03. Meteor & React For Everyone/myResolutions/client/layouts/MainLayout.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+{/* content = Parameter, wie bei einer Funktion */}
+export const MainLayout = ({content}) => (
+  <div className='main-layout'>
+    {content}
+  </div>
+)
