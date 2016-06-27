90e593ddb3a33184202e1aa338f26d02132fa265
Michael Seelisch <seelisch@kolle-rebbe.de>
2016-06-27 16:22:20 +0200

    Added project "Getting started with React Router"

---
+++ React/09. Getting Started with React Router/src/App_03_IndexRoute.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Router, Route, Link, IndexRoute, browserHistory} from 'react-router';
+
+const Outer = (props) => <div><h1>Our Site</h1><Links />{ props.children }</div>;
+const About = () => <div><h1>About</h1><Links /></div>;
+const Contact = () => <div><h1>Contact</h1><Links /></div>;
+
+const Links = () =>
+  <nav>
+    <Link activeStyle={{ color: 'green'}} to="/">Home</Link>
+    <Link activeStyle={{ color: 'green'}} to="/about">About</Link>
+    <Link activeClassName='active' to="/contact">Contact</Link>
+  </nav>
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router history={ browserHistory }>
+        <Route path="/" component={ Outer }>
+          <IndexRoute component={ About }></IndexRoute>
+          <Route path="contact" component={ Contact }></Route>
+        </Route>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ React/09. Getting Started with React Router/src/App_07_Redirect.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Router, Route, Redirect, Link, hashHistory} from 'react-router';
+
+const Home = () => <div><h1>Home</h1><Links /></div>;
+const About = () => <div><h1>About Us</h1><Links /></div>;
+const Contact = () => <div><h1>Contact</h1><Links /></div>;
+
+const Links = () =>
+  <nav>
+    <Link activeStyle={{ color: 'green'}} to="/">Home</Link>
+    <Link activeStyle={{ color: 'green'}} to="/about-us">About Us</Link>
+    <Link activeStyle={{ color: 'green'}} to="/about">About</Link>
+    <Link activeClassName='active' to="/contact">Contact</Link>
+  </nav>
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router history={ hashHistory }>
+        <Route path="/" component={ Home }></Route>
+        <Route path="/about-us" component={ About }></Route>
+        <Route path="/contact" component={ Contact }></Route>
+        <Redirect from="/about" to="/about-us"></Redirect>
+      </Router>
+    );
+  }
+}
+
+export default App;
