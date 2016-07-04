e8ce89a1c2e2f7f45a6326f1c34f0102ce37f337
Michael Seelisch <seelisch@kolle-rebbe.de>
2016-07-04 14:47:31 +0200

    Finsihing project "Learn Redux with Wes Bos"

--- Redux/03. Learn Redux with Wes Bos/client/components/Single.js
+++ Redux/03. Learn Redux with Wes Bos/client/components/Single.js
@@ -5,15 +5,15 @@ import Comments from './Comments';
 
 const Single = React.createClass({
   render() {
-    const {postId} = this.props.params.postId;
+    const {postId} = this.props.params;
     const i = this.props.posts.findIndex((post) => post.code === postId)
     const post = this.props.posts[i];
     const postComments = this.props.comments[postId] || [];
 
     return(
-      <div className="photo-grid">
+      <div className="single-photo">
         <Photo i={i} post={post} {...this.props} />
-        <Comments postComments={postComments} />
+        <Comments postComments={postComments} {...this.props} />
       </div>
     )
   }
--- Redux/03. Learn Redux with Wes Bos/client/reducers/comments.js
+++ Redux/03. Learn Redux with Wes Bos/client/reducers/comments.js
@@ -2,8 +2,45 @@
 // 1. The action (info about what happened)
 // 2. Copy about current state
 
+function postComments(state = [], action) {
+  // Return the newstate with the new comment
+  switch(action.type) {
+    case 'ADD_COMMENT':
+      return [...state, {
+          user: action.author,
+          text: action.comment
+        }
+      ]
+      break;
+
+    case 'REMOVE_COMMENT':
+    console.log('Removing a comment!');
+    // We need to return the new state without the deleted comment
+      return [
+        // From the start to the one we want to delete
+        ...state.slice(0, action.i),
+
+        // After the deleted one, to the end
+        ...state.slice(action.i + 1)
+      ]
+
+    default:
+      return state;
+      break;
+  }
+  return state;
+}
+
 function comments(state = [], action) {
-  
+  if(typeof action.postId !== 'undefined') {
+    return {
+      // Take the current state
+      ...state,
+
+      // Overwrite thispost with a new one
+      [action.postId]: postComments(state[action.postId], action)
+    }
+  }
   return state;
 }
 
