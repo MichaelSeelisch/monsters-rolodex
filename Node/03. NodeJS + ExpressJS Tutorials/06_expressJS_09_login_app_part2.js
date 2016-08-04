05e3cda265f1f527be50add6c194799a46e0de12
Michael Seelisch <seelisch@kolle-rebbe.de>
2016-08-04 15:08:20 +0200

    Update project "NodeJS + ExpressJS Tutorials"

--- Node/03. NodeJS + ExpressJS Tutorials/06_expressJS_09_login_app_part2.js
+++ Node/03. NodeJS + ExpressJS Tutorials/06_expressJS_09_login_app_part2.js
@@ -5,7 +5,7 @@ var sessions = require('express-session');
 var session;
 
 var app = express();
-
+app.use('/cssFiles', express.static(__dirname + '/assets'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(sessions({
@@ -32,9 +32,9 @@ app.post('/login', function(req, resp) {
     resp.redirect/('/redirects');
   }
 
-  if(req.body.username == 'admin' && req.body.password == 'admin') {
+  // if(req.body.username == 'admin' && req.body.password == 'admin') {
     session.uniqueID = req.body.username;
-  }
+  // }
 
   resp.redirect('/redirects');
 });
@@ -47,18 +47,25 @@ app.get('/logout', function(req, resp) {
   });
 });
 
+app.get('/admin', function(req, resp) {
+  session = req.session;
+
+  if(session.uniqueID != 'admin') {
+    resp.end('Unauthorized access!');
+  }
+  else {
+    resp.send('You are the admin! <a href="/logout">KILL SESSION</a>');
+  }
+});
+
 app.get('/redirects', function(req, resp) {
   session = req.session;
 
-  if(session.uniqueID) {
-    resp.redirect/('/redirects');
-  }
-
-  if(session.uniqueID) {
+  if(session.uniqueID == 'admin') {
     resp.redirect('/admin');
   }
   else {
-    resp.send('Who are you?? <a href="/logout">KILL SESSION</a>');
+    resp.send(req.session.uniqueID + ' not found <a href="/logout">KILL SESSION</a>');
   }
 });
 
