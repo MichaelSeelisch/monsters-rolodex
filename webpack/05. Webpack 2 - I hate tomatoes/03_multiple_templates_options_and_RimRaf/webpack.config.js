6e0c8a79d628830d17283fec9cf3360937950197
Michael Seelisch <seelisch@kolle-rebbe.de>
2017-03-07 17:35:55 +0100

    Update project “Webpack 2”

---
+++ webpack/05. Webpack 2 - I hate tomatoes/03_multiple_templates_options_and_RimRaf/webpack.config.js
@@ -0,0 +1,62 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var path = require('path');
+
+module.exports = {
+    entry: {
+        app: './src/app.js',
+        contact: './src/contact'  
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true, // gzip compression
+        port: 9000,
+        stats: 'errors-only',
+        open: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallbackLoader: 'style-loader', 
+                    loader: ['css-loader', 'sass-loader'],
+                    publicPath: './dist'
+                })
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Project Demo',
+            /*minify: {
+                collapseWhitespace: true
+            },*/
+            hash: true,
+            excludeChunks: ['contact'],     // Exclude contact.js code
+            /* filename: './../index.html', */
+            template: './src/index.html'
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Contact Page',
+            hash: true,
+            chunks: ['contact'],    // Only inlcude contact.js code
+            filename: 'contact.html',
+            template: './src/contact.html'
+        }),
+        new ExtractTextPlugin({
+            filename: 'app.css',
+            disable: false,
+            allChunks: true
+        })
+    ]
+}
---
+++ webpack/05. Webpack 2 - I hate tomatoes/README.md
@@ -0,0 +1,3 @@
+Tutorial from 'ihatetomatoes':
+
+https://www.youtube.com/watch?v=JdGnYNtuEtE&list=PLkEZWD8wbltnRp6nRR8kv97RbpcUdNawY
