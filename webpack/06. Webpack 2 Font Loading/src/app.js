a656d68d4eba1a8a601ba31de9d7cb7fe5a8785c
Michael Seelisch <seelisch@kolle-rebbe.de>
2017-03-21 18:12:46 +0100

    Added project „Webpack 2 Font Loading“

---
+++ webpack/06. Webpack 2 Font Loading/src/app.js
@@ -0,0 +1,4 @@
+import font from './myCustomFont.scss';
+import css from './app.scss';
+
+console.log('Testing Hot Module Replacement!');
---
+++ webpack/06. Webpack 2 Font Loading/webpack.config.js
@@ -0,0 +1,75 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var webpack = require('webpack');
+var path = require("path");
+
+var isProd = process.env.NODE_ENV == 'production'; // true or false
+var cssDev = ['style-loader', 'css-loader','sass-loader'];
+var cssProd = ExtractTextPlugin.extract({
+    fallback: 'style-loader',
+    loader: ['css-loader','sass-loader'],
+    publicPath: '/dist'
+});
+var cssConfig = isProd ? cssProd : cssDev;
+
+module.exports = {
+    entry: {
+        app: './src/app.js',
+        contact: './src/contact'  
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: cssConfig
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.(svg|woff|woff2|eot|ttf)(\?.*$|$)/,
+                loader: 'file'
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        hot: true,
+        stats: "errors-only",
+        open: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Project Demo',
+            /*minify: {
+                collapseWhitespace: true
+            },*/
+            hash: true,
+            excludeChunks: ['contact'],     // Exclude contact.js code
+            /* filename: './../index.html', */
+            template: './src/index.html'
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Contact Page',
+            hash: true,
+            chunks: ['contact'],    // Only inlcude contact.js code
+            filename: 'contact.html',
+            template: './src/contact.html'
+        }),
+        new ExtractTextPlugin({
+            filename: 'app.css',
+            disable: true,
+            allChunks: true
+        }),
+        new webpack.HotModuleReplacementPlugin(), // enable HMR globally
+
+        new webpack.NamedModulesPlugin(), // prints more readable module names in the browser console on HMR updates
+    ]
+}
