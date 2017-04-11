ce5450dc10ff29d6a342e77575d182c22cb0525a
Michael Seelisch <seelisch@kolle-rebbe.de>
2017-04-11 17:15:55 +0200

    Update project "Learning Vue"

---
+++ Vue/01. Learning Vue.js/chapter_04/02_expressions_and_filters/src/components/StateTitleComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h3>{{ isWorking ? workingTitle : restingTitle }}</h3>
+    <p>
+      {{ Math.pow(5, 2) }}
+    </p>
+  </div>
+</template>
+
+<style scoped>
+</style>
+
+<script>
+  export default {
+    data () {
+      return {
+        workingTitle: 'Work!',
+        restingTitle: 'Rest!'
+      }
+    },
+    props: ['isWorking']
+  }
+</script>
+
