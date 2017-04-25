e698f0ae7a65896d33479448b70cf018eccbe9eb
Michael Seelisch <seelisch@kolle-rebbe.de>
2017-04-25 17:36:49 +0200

    Update project "Learning Vue"

---
+++ Vue/01. Learning Vue.js/chapter_04/06_list_of_shopping_lists_AddItemComponent_eventListener/src/components/ShoppingListComponent.vue
@@ -0,0 +1,40 @@
+<template lang="jade">
+div
+  h2 {{ title }}
+  add-item-component(:items='items', @add='addItem')
+  items-component(:items='items')
+  .footer
+    hr
+    em Change the title of your shopping list here
+    input(v-model="title")
+</template>
+
+<script>
+  import AddItemComponent from './AddItemComponent'
+  import ItemsComponent from './ItemsComponent'
+  import ChangeTitleComponent from './ChangeTitleComponent'
+
+  export default {
+    components: {
+      AddItemComponent,
+      ItemsComponent,
+      ChangeTitleComponent
+    },
+    props: ['title', 'items'],
+    methods: {
+      addItem (text) {
+        this.items.push({
+          text: text,
+          checked: false
+        })
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .footer {
+    font-size: 0.7em;
+    margin-top: 40vh;
+  }
+</style>
