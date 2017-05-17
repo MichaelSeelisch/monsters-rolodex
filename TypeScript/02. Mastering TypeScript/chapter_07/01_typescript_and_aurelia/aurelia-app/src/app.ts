f58a18336d791457a2c33ae3e43ab275c7855618
Michael Seelisch <seelisch@kolle-rebbe.de>
2017-05-17 17:42:13 +0200

    Update project „Mastering TypeScript“

--- TypeScript/02. Mastering TypeScript/chapter_07/01_typescript_and_aurelia/aurelia-app/src/app.ts
+++ TypeScript/02. Mastering TypeScript/chapter_07/01_typescript_and_aurelia/aurelia-app/src/app.ts
@@ -1,3 +1,13 @@
 export class App {
-  message = 'Hello World!';
+  message: string = 'Select an Option!';
+  items: ClickableItem[] = [
+    { id: 1, displayName: 'firstItem' },
+    { id: 2, displayName: 'secondItem' },
+    { id: 3, displayName: 'thirdItem' },
+  ];
+}
+
+export class ClickableItem {
+  displayName: string;
+  id: number;
 }
