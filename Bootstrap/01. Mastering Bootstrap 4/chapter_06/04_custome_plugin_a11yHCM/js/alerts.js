e934458eea32c114454498117ca5b93f8fd0f7a5
Michael Seelisch <seelisch@kolle-rebbe.de>
2017-05-18 18:43:07 +0200

    Update project “Mastering Bootstrap”

---
+++ Bootstrap/01. Mastering Bootstrap 4/chapter_06/04_custome_plugin_a11yHCM/js/alerts.js
@@ -0,0 +1,52 @@
++function ($) {
+  'use strict';
+  
+  var Alert = $.fn.alert.Constructor;
+  
+  Alert.prototype.click = function (e) {
+    var $this = $(this)
+    var dataAttr;
+    $.each($($this).data(), function(key, value) {
+      dataAttr = key.toString(); 
+    })
+    var selector = $this.attr('data-' + dataAttr)
+
+    if (!selector) {
+      selector = $this.attr('href')
+      selector = selector && selector.replace(/.*(?=#[^\s]*$)/, '') // strip for ie7
+    }
+    
+    $this.addClass('hidden-sm-up')
+
+    var $parent = $(selector)
+    
+    if (e) e.preventDefault()
+
+    if (!$parent.length) {
+      $parent = $this.closest('.alert')
+    }
+
+    console.log(dataAttr);
+
+    switch (dataAttr) {
+      case 'expand':
+        $this.siblings('.minimize').removeClass('hidden-sm-up')
+        $parent.trigger(e = $.Event('expand.bs.alert'))
+        $parent.removeClass('alert-minimize')
+        break;
+
+      case 'minimize':
+      default:
+        $this.siblings('.expand').removeClass('hidden-sm-up')
+        $parent.trigger(e = $.Event('minimize.bs.alert'))
+        $parent.addClass('alert-minimize')
+        break;
+    }
+
+    if (e.isDefaultPrevented()) return
+  }
+ 
+  $(document).on('click.bs.alert.data-api', '[data-minimize="alert"]', Alert.prototype.click)
+  $(document).on('click.bs.alert.data-api', '[data-expand="alert"]', Alert.prototype.click)
+
+}(jQuery);
