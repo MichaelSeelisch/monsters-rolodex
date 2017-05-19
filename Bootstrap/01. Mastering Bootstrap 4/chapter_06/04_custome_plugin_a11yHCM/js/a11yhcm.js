e738113b09c7ec87391a041df1e9c2e43a0ecd47
Michael Seelisch <seelisch@kolle-rebbe.de>
2017-05-19 14:47:14 +0200

    Update project “Mastering Bootstrap”

--- Bootstrap/01. Mastering Bootstrap 4/chapter_06/04_custome_plugin_a11yHCM/js/a11yhcm.js
+++ Bootstrap/01. Mastering Bootstrap 4/chapter_06/04_custome_plugin_a11yHCM/js/a11yhcm.js
@@ -1,72 +1,78 @@
 + function ($) {      
-  'use strict';       // A11YHCM CLASS DEFINITION
-       // ======================
+  'use strict';
+  
+  // A11YHCM CLASS DEFINITION
+  // ======================
         
   var A11yHCM = function (element) {      
-    this.$element = $(element)  
-  }  
-  A11yHCM.VERSION = '1.0.0'  
+    $(element).on('click', '[data-a11yhcm]', this.toggle)
+  }
+    
+  A11yHCM.VERSION = '1.0.0'
+    
   A11yHCM.DEFAULTS = {    
     styleSheetID: 'bs-a11yhcm'  
-  }  
-  A11yHCM.prototype.toggle = function (e) {      
+  }
+    
+  A11yHCM.prototype.toggle = function (e) { 
+         
       var $this = $(this)
       var styleSheet = $this.attr('data-a11yhcm')
       
-      if ($this.attr('a11yhcm-id')) $this.styleSheetID = $this.attr('a11yhcm-id')  
-      else $this.styleSheetID = A11yHCM.DEFAULTS.styleSheetID   
-      if (document.getElementById($this.styleSheetID))
-        $('#' + $this.styleSheetID).remove()
-      else {
-        var styleSheetLink = '<link href="' + styleSheet + 
-        '"rel = "stylesheet" id = "' + $this.styleSheetID + '" / > '
-        $('head').append(styleSheetLink)  
+      if ($this.attr('a11yhcm-id')) {
+        $this.styleSheetID = $this.attr('a11yhcm-id') 
       }
-}
-}(jQuery);
+      else {
+        $this.styleSheetID = A11yHCM.DEFAULTS.styleSheetID
+      }
+        
+      if (document.getElementById($this.styleSheetID)) {
+        $('#' + $this.styleSheetID).remove()
+        $this.removeClass('enabled')
+      }
+      else {
+        var styleSheetLink = '<link href="' + styleSheet + '" rel="stylesheet" id="' + $this.styleSheetID + '"/>'
+        $('head').append(styleSheetLink)
+        $this.addClass('enabled') 
+      }
+  }
 
-// A11YHCM PLUGIN DEFINITION
-// =======================
-function Plugin(option) {
-  return this.each(function () {
-    var $this = $(this)
-    var data = $this.data('bs.a11yhcm')
-    if (!data) $this.data('bs.a11yhcm', (data = new A11yHCM(this)))
-    if (typeof option == 'string') data[option].call($this)
+  // A11YHCM PLUGIN DEFINITION
+  // =======================
+  function Plugin(option) {
+    return this.each(function () {
+      var $this = $(this)
+      var data = $this.data('bs.a11yhcm')
+
+      if (!data) {
+        $this.data('bs.a11yhcm', (data = new A11yHCM(this)))
+      }
+      if (typeof option == 'string') {
+        data[option].call($this)
+      }
+    })
+  }
+
+  var old = $.fn.a11yhcm
+
+  $.fn.a11yhcm = Plugin
+  $.fn.a11yhcm.Constructor = A11yHCM
+
+  // A11YHCM NO CONFLICT
+  // =================
+  $.fn.a11yhcm.noConflict = function () {
+    $.fn.a11yhcm = old
+    return this
+  }
+
+  // A11YHCM DATA-API
+  // ==============
+  $(document).on('click.bs.allyhcm.data-api', '[data-a11yhcm]', A11yHCM.prototype.toggle)
+
+  $(window).on('load', function () {    
+    $('[data-a11yhcm]').each(function () {         
+      var $a11yhcm = $(this)
+      Plugin.call($a11yhcm, $a11yhcm.data())    
+    })  
   })
-}
-
-var old = $.fn.a11yhcm
-$.fn.a11yhcm = Plugin
-$.fn.a11yhcm.Constructor = A11yHCM
-
-// A11YHCM NO CONFLICT
-// =================
-$.fn.a11yhcm.noConflict = function () {
-  $.fn.a11yhcm = old
-  return this
-}
-
-// A11YHCM DATA-API
-// ==============
-$(document).on('click.bs.allyhcm.data-api', '[data-a11yhcm]', A11yHCM.prototype.toggle)
-
-$(window).on('load', function () {    
-  $('[data-a11yhcm]').each(function () {         
-    var $a11yhcm = $(this)
-    Plugin.call($a11yhcm, $a11yhcm.data())    
-  })  
-})
-
-A11yHCM.prototype.toggle = function (e) {      
-  var $this = $(this)
-  var styleSheet = $this.attr('data-a11yhcm')  
-}
-
-if (document.getElementById('bs-a11yhcm'))
-  $('#' + $this.styleSheetID).remove()   
-else {      
-  var styleSheetLink = '<link href="' + styleSheet +
-  '"rel = "stylesheet" id = "bs-a11yhcm" / > '    
-  $('head').append(styleSheetLink)  
-}
+}(jQuery);
