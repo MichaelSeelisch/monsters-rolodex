c59311a6a403616111b38cc0c363745439ab37dc
Michael Seelisch <m.seelisch@superreal.de>
2017-09-01 10:15:17 +0200

    Revert "Adde project "Gulp - Simple Setup""
    This reverts commit 424769e6b3463e9c71508c2264eff797adf4f175.

--- 00 Boilerplates/04. Gulp/01. Simple Setup/src/gulp/tasks/default.js
+++ 00 Boilerplates/04. Gulp/01. Simple Setup/src/gulp/tasks/default.js
@@ -3,16 +3,11 @@
 const config = require('../config');
 
 const gulp = require('gulp');
-const util = require('gulp-util');
 
-gulp.task('watch', function () {
+gulp.task('watch', function(cb) {
   gulp.watch(config.paths.styles.all, ['styles']);
   gulp.watch(config.paths.scripts.all, ['scripts']);
   gulp.watch(config.paths.images.all, ['images']);
 });
 
-if(util.env.production) {
-  gulp.task('default', ['styles', 'scripts', 'images', 'analyze', 'server', 'watch']);
-} else {
-  gulp.task('default', ['styles', 'scripts', 'images', 'server', 'watch']);
-}
+gulp.task('default', ['styles', 'scripts', 'images', 'analyze', 'server', 'watch']);
