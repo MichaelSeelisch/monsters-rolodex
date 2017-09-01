424769e6b3463e9c71508c2264eff797adf4f175
Michael Seelisch <m.seelisch@superreal.de>
2017-09-01 10:14:48 +0200

    Adde project "Gulp - Simple Setup"

--- 00 Boilerplates/04. Gulp/01. Simple Setup/src/gulp/tasks/default.js
+++ 00 Boilerplates/04. Gulp/01. Simple Setup/src/gulp/tasks/default.js
@@ -3,11 +3,16 @@
 const config = require('../config');
 
 const gulp = require('gulp');
+const util = require('gulp-util');
 
-gulp.task('watch', function(cb) {
+gulp.task('watch', function () {
   gulp.watch(config.paths.styles.all, ['styles']);
   gulp.watch(config.paths.scripts.all, ['scripts']);
   gulp.watch(config.paths.images.all, ['images']);
 });
 
-gulp.task('default', ['styles', 'scripts', 'images', 'analyze', 'server', 'watch']);
+if(util.env.production) {
+  gulp.task('default', ['styles', 'scripts', 'images', 'analyze', 'server', 'watch']);
+} else {
+  gulp.task('default', ['styles', 'scripts', 'images', 'server', 'watch']);
+}
