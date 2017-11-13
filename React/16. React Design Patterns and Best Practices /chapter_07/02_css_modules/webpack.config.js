97ecd95608f962eb84bd6d3045c50c8f24ca1d58
Michael Seelisch <info@michaelseelisch.de>
2017-11-13 11:35:55 +0100

    Update project „React Design Patterns“

--- React/16. React Design Patterns and Best Practices /chapter_07/02_css_modules/webpack.config.js
+++ React/16. React Design Patterns and Best Practices /chapter_07/02_css_modules/webpack.config.js
@@ -1,24 +1,24 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
+
   module: {
-    rules: [
-        {
-          test: /\.js$/, 
-          exclude: /(node_modules|bower_components)/, 
-          loader: 'babel-loader', 
-          options: {
-            presets: ['es2015', 'react']
-          } 
-        }, 
-        { 
-          test: /\.css$/, 
-          loader: 'style-loader', 
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react'],
         },
-    ]
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css?modules&localIdentName=[local]--[hash:base64:5]',
+      },
+    ],
   },
-  plugins: [
-    new HtmlWebpackPlugin()
-  ]
-};
+
+  plugins: [new HtmlWebpackPlugin()],
+}
