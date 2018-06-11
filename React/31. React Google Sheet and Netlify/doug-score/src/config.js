1f038d5eecdaa9fbd5ea30c0a819cca41a4d9b03
Michael Seelisch <m.seelisch@superreal.de>
2018-06-11 11:24:26 +0200

    Add project "React, Google Sheet and Netlify"

---
+++ React/31. React Google Sheet and Netlify/doug-score/src/config.js
@@ -0,0 +1,5 @@
+export default {
+    apiKey: 'AIzaSyDNivIN5eueHHFj5f6OkfHgQCD3cmrOVEA',
+    discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+    spreadsheetId: '1KTArYwDWrn52fnc7B12KvjRb6nmcEaU6gXYehWfsZSo'
+  };
