b3d3d52e130027c0e5543eb08c7067c5a3f12781
Michael Seelisch <m.seelisch@superreal.de>
2018-06-18 10:13:11 +0200

    Update project "Advanced Vue Component Design"

--- Vue/07. Advanced Vue Component Design/#01 Building Controlled Components/src/components/ToggleInput.vue
+++ Vue/07. Advanced Vue Component Design/#01 Building Controlled Components/src/components/ToggleInput.vue
@@ -1,18 +1,22 @@
 <template>
-  <span class="toggle" role="checkbox" tabindex="0"
-    @click="toggle"
-    @keydown.space.prevent="toggle"
-    :aria-checked="value.toString()"
-  ></span>
+    <span 
+        class="toggle" 
+        role="checkbox" 
+        tabindex="0"
+        @click="toggle"
+        @keydown.space.prevent="toggle"
+        :aria-checked="value.toString()"
+    ></span>
 </template>
 
 <script>
-export default {
-  props: ["value"],
-  methods: {
-    toggle() {
-      this.$emit("input", !this.value)
+    export default {
+        props: ["value"],
+        
+        methods: {
+            toggle() {
+                this.$emit("input", !this.value)
+            }
+        }
     }
-  }
-}
 </script>
