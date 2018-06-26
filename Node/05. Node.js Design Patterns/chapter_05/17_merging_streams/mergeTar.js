c2e9a5dd2ddee651a0a0caa8250f67c1521fe655
Michael Seelisch <m.seelisch@superreal.de>
2018-06-26 10:16:56 +0200

    Update project "Node.js Design Patterns"

---
+++ Node/05. Node.js Design Patterns/chapter_05/17_merging_streams/mergeTar.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const tar = require('tar');
+const fstream = require('fstream');
+const path = require('path');
+
+const destination = path.resolve(process.argv[2]);
+const sourceA = path.resolve(process.argv[3]);
+const sourceB = path.resolve(process.argv[4]);
+
+const pack = new tar.Pack();
+pack.pipe(fstream.Writer(destination));
+
+let endCount = 0;
+function onEnd() {
+  if(++endCount === 2) {
+    pack.end();
+  }
+}
+
+const sourceStreamA = fstream.Reader({type: "Directory", path: sourceA})
+    .on('error', (err) => { console.log(err.message) })
+    .on('end', onEnd)
+;
+
+const sourceStreamB = fstream.Reader({type: "Directory", path: sourceB})
+    .on('error', (err) => { console.log(err.message) })
+    .on('end', onEnd)
+;
+
+sourceStreamA.pipe(pack, {end: false});
+sourceStreamB.pipe(pack, {end: false});
