317f32b78acfccc185faa0224e15ab65e239b6e3
Michael Seelisch <m.seelisch@superreal.de>
2018-07-05 09:47:26 +0200

    Update project "Node.js Design Patterns"

---
+++ Node/05. Node.js Design Patterns/chapter_06/01_simple_factory/image_factory.js
@@ -0,0 +1,13 @@
+function createImage(name) {
+    if (name.match(/\.jpeg$/)) {
+        return new JpegImage(name);
+    } else if (name.match(/\.gif$/)) {
+        return new GifImage(name);
+    } else if (name.match(/\.png$/)) {
+         return new PngImage(name);
+    } else {
+        throw new Exception('Unsupported format');
+    }
+}
+
+const image = createImage('photo.jpeg');
