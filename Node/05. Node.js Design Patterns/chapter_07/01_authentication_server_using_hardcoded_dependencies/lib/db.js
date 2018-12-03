bed479c91064d42f2cac7e2db18eaf3e97814e8c
Michael Seelisch <m.seelisch@superreal.de>
2018-12-03 10:32:46 +0100

    Update project "Node.js Design Patterns"

---
+++ Node/05. Node.js Design Patterns/chapter_07/01_authentication_server_using_hardcoded_dependencies/lib/db.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const level = require('level');
+const sublevel = require('level-sublevel');
+
+module.exports = sublevel(
+    level('example-db', {
+        valueEncoding: 'json'
+    })
+);
