12a70db755604dba5d51420367b6fc352ae42e24
Michael Seelisch <m.seelisch@superreal.de>
2019-02-28 14:05:00 +0100

    Add project "Lazy Loading Components"

---
+++ Vue/17. Lazy Loading Components/src/router.js
@@ -0,0 +1,22 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+Vue.use(Router);
+
+function loadView(view) {
+    return () => import(/* webpackChunkName: "view-[request]" */ `@/views/${view}.vue`)
+}
+
+export default new Router({
+    routes: [
+    {
+        path: '/',
+        name: 'home',
+        component: loadView('Home')
+    },
+    {
+        path: '/about',
+        name: 'about',
+        component: loadView('About')
+    }]
+});
