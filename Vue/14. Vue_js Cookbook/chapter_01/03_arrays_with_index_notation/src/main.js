4340b89f9d3eaad756aaa34b6e7591525c2a543b
Michael Seelisch <m.seelisch@superreal.de>
2019-04-04 10:05:35 +0200

    Update project "Vue_js Cookbook"

---
+++ Vue/14. Vue_js Cookbook/chapter_01/03_arrays_with_index_notation/src/main.js
@@ -0,0 +1,7 @@
+new Vue({
+    el: '#app',
+    data: {
+      animals: ['dog', 'cat', 'bird'],
+      sounds: ['woof', 'meow', 'tweet']
+    }
+});
---
+++ Vue/14. Vue_js Cookbook/chapter_01/04_objects/src/main.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: '#app',
+    data: {
+      animals: {
+        dog: 'woof',
+        cat: 'meow',
+        bird: 'tweet'
+      }
+    }
+});
