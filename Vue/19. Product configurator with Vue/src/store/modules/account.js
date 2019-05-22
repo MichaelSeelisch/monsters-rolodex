2b1dae0adf1854c1c0a0649a841f91376450ca7c
Michael Seelisch <m.seelisch@superreal.de>
2019-05-22 17:22:52 +0200

    Add project "Product configurator with Vue"

---
+++ Vue/19. Product configurator with Vue/src/store/modules/account.js
@@ -0,0 +1,43 @@
+const state = {
+    auth: false,
+    showLoginForm: false,
+};
+
+const actions = {
+    showLogin({ commit }) {
+        commit('showForm');
+    },
+    hideLogin({ commit }) {
+        commit('hideForm');
+    },
+    attemptLogin({ commit }) {
+        // not when empty => need to add stuff to state
+        commit('authorize');
+    },
+    logout({ commit }) {
+        commit('deauthorize');
+    },
+};
+
+const mutations = {
+    showForm(state) {
+        state.showLoginForm = true;
+    },
+    hideForm(state) {
+        state.showLoginForm = false;
+    },
+    authorize(state) {
+        state.auth = true;
+        state.showLoginForm = false;
+    },
+    deauthorize(state) {
+        state.auth = false;
+    },
+}
+
+export default {
+    namespaced: true,
+    state,
+    actions,
+    mutations,
+};
