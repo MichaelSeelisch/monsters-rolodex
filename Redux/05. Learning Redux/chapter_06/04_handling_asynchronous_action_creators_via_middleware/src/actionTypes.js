859300d881dce74637e51d4d26177e1029c33d8f
Michael Seelisch <m.seelisch@superreal.de>
2019-06-12 17:07:34 +0200

    Update project "Learning Redux"

---
+++ Redux/05. Learning Redux/chapter_06/04_handling_asynchronous_action_creators_via_middleware/src/actionTypes.js
@@ -0,0 +1,16 @@
+// users
+export const CREATE_USER = 'CREATE_USER'
+
+// posts
+export const CREATE_POST = 'CREATE_POST'
+export const EDIT_POST = 'EDIT_POST'
+export const DELETE_POST = 'DELETE_POST'
+
+export const FETCH_POSTS_REQUEST = 'FETCH_POSTS_REQUEST'
+export const FETCH_POSTS_SUCCESS = 'FETCH_POSTS_SUCCESS'
+export const FETCH_POSTS_FAILURE = 'FETCH_POSTS_FAILURE'
+
+// filter
+export const SET_FILTER = 'SET_FILTER'
+export const CLEAR_FILTER = 'CLEAR_FILTER'
+
