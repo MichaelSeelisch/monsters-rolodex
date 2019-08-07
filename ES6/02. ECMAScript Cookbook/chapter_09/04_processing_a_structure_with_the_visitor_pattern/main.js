b4a89b914adedbbd6a6a46395a41899f98804f1e
Michael Seelisch <info@michaelseelisch.de>
2019-08-07 18:38:34 +0200

    Update project "ECMAScript Cookbook"

---
+++ ES6/02. ECMAScript Cookbook/chapter_09/04_processing_a_structure_with_the_visitor_pattern/main.js
@@ -0,0 +1,177 @@
+class Mission { 
+    constructor (name) { 
+        this.name = name; 
+    } 
+   
+    describe () { 
+        console.log(`
+            The ${this.name} mission will be launched by a
+            ${this.rocket.name} rocket, and deliver a ${this.payload.name} to
+            ${this.destination.name}. 
+        `); 
+    }
+
+    accept (visitor) { 
+        this.rocket.accept(visitor); 
+        this.payload.accept(visitor); 
+        this.destination.accept(visitor); 
+        visitor.visit(this);
+    }
+}
+
+class Destination { 
+    constructor (name) { 
+        this.name = name; 
+    }
+
+    accept (visitor) { 
+        visitor.visit(this); 
+    }
+} 
+   
+class Payload { 
+    constructor (name) { 
+        this.name = name; 
+    }
+
+    accept (visitor) { 
+        visitor.visit(this); 
+    }
+} 
+   
+class Rocket { 
+    constructor (name) { 
+        this.name = name; 
+    }
+
+    accept (visitor) { 
+        visitor.visit(this); 
+    }
+}
+
+class MissionBuilder {
+    build () { 
+        const mission = new Mission(this.missionName); 
+        mission.rocket = this.rocket; 
+        mission.destination = this.destination; 
+        mission.payload = this.payload;
+        return mission; 
+    } 
+
+    setMissionName (name) { 
+        this.missionName = name; 
+        return this; 
+    } 
+   
+    setDestination (destination) { 
+        this.destination = destination;
+        console.log(this);
+        return this; 
+    } 
+   
+    setPayload (payload) { 
+        this.payload = payload; 
+        return this; 
+    } 
+   
+    setRocket (rocket) { 
+        this.rocket = rocket; 
+        return this; 
+    } 
+}
+
+export function main() { 
+    // build an describe a mission
+    const jadeRabbit = new MissionBuilder() 
+        .setMissionName('Jade Rabbit') 
+        .setDestination(new Destination('Moon')) 
+        .setPayload(new Payload('Lunar Rover')) 
+        .setRocket(new Rocket('Long March 3B Y-23')) 
+        .build(); 
+
+    const curiosity = new MissionBuilder() 
+        .setMissionName('Curiosity') 
+        .setDestination(new Destination('Mars'))
+        .setPayload(new Payload('Mars Rover')) 
+        .setRocket(new Rocket('Delta II')) 
+        .build(); 
+ 
+    // expect error from Destination 
+    const buzz = new MissionBuilder() 
+        .setMissionName('Buzz Lightyear') 
+        .setDestination(new Destination('Too Infinity And Beyond')) 
+        .setPayload(new Payload('Interstellar Orbiter')) 
+        .setRocket(new Rocket('Self Propelled')) 
+        .build(); 
+ 
+    // expect error from payload 
+    const terraformer = new MissionBuilder() 
+        .setMissionName('Mars Terraformer') 
+        .setDestination(new Destination('Mars')) 
+        .setPayload(new Payload('Terraformer')) 
+        .setRocket(new Rocket('Light Sail')) 
+        .build(); 
+ 
+    const inspector = new MissionInspector(); 
+    
+    [jadeRabbit, curiosity, buzz, terraformer].forEach((mission) => 
+    { 
+        try { 
+            mission.accept(inspector); 
+        } catch (e) { console.error(e); } 
+    });
+}
+
+/* visitor that inspects mission */ 
+class MissionInspector {
+    visitRocket (rocket) { 
+        console.log('Rocket: ', rocket.name, ' approved'); 
+    }
+
+    visitPayload (payload) { 
+        const name = payload.name.toLowerCase(); 
+        const payloadExpr = /(orbiter)|(rover)/; 
+     
+        if ( payloadExpr.test(name) ) { 
+            console.log('Payload: ', name, ' approved'); 
+        } else { 
+            throw new Error('Payload: ' + name + 'not approved at this time'); 
+        }  
+    }
+  
+    visitDestination (destination) { 
+        const name = destination.name.toLowerCase();
+
+        if ( 
+            name === 'mercury' || 
+            name === 'venus' || 
+            name === 'earth' || 
+            name === 'moon' || 
+            name === 'mars' 
+        ) { 
+            console.log('Destination: ', name, ' approved'); 
+        } else { 
+            throw new Error('Destination: ' + name + 'not approved at this time'); 
+        } 
+    } 
+   
+    visitMission (mission) { 
+        console.log('Mission ok'); 
+        mission.describe(); 
+    }
+    
+    visit (element) { 
+        if (element instanceof Mission) { 
+            this.visitMission(element); 
+        } 
+        else if (element instanceof Destination) { 
+            this.visitDestination(element); 
+        } 
+        else if (element instanceof Rocket) { 
+            this.visitRocket(element); 
+        } 
+        else if (element instanceof Payload) { 
+            this.visitPayload(element); 
+        } 
+    }
+}
