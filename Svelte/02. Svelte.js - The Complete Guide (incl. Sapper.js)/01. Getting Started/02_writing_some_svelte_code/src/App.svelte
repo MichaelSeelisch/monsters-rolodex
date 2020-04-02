886265a10d4a93be912c271b82cbf92ab816ce9a
Michael Seelisch <info@michaelseelisch.de>
2020-04-02 21:06:24 +0200

    Add project "Svelte.js - The Complete Guide (incl. Sapper.js)"

---
+++ Svelte/02. Svelte.js - The Complete Guide (incl. Sapper.js)/01. Getting Started/02_writing_some_svelte_code/src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	let name = 'Micha';
+	let age = 42;
+
+	function incrementAge() {
+		age += 1;
+	}
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {name}, your age is {age}!</h1>
+<button on:click='{incrementAge}'>Change Age</button>
