7ca46e12a0dc438eded7ad65206d8e322e6a4d54
Michael Seelisch <info@michaelseelisch.de>
2020-06-16 20:51:01 +0200

    Add project "Create a state-based UI component with vanilla JS"

---
+++ JavaScript General/17. Create a state-based UI component with vanilla JS/index.js
@@ -0,0 +1,32 @@
+var Rue = function (options) {
+	this.elem = document.querySelector(options.selector);
+	this.data = options.data;
+	this.template = options.template;
+};
+
+Rue.prototype.render = function () {
+	this.elem.innerHTML = this.template(this.data);
+};
+
+var app = new Rue({
+	selector: '#app',
+	data: {
+		heading: 'My Todos',
+		todos: ['Swim', 'Climb', 'Jump', 'Play']
+	},
+	template: function (props) {
+		return `
+			<h1>${props.heading}</h1>
+			<ul>
+				${props.todos.map(function (todo) {
+					return `<li>${todo}</li>`;
+				}).join('')}
+			</ul>`;
+	}
+});
+
+// Add a new item to the data
+app.data.todos.push('Take a nap... zzzzz');
+
+// Render an updated UI
+app.render();
