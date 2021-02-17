ffc3394210faed4bd6e485da9f6a79127c65d016
Michael Seelisch <info@michaelseelisch.de>
2021-02-17 23:09:45 +0100

    Add project "Complete React Developer inn 2021"

---
+++ React/43. Complete React Developer in 2021 (w: Redux, Hooks, GraphQL)/03. React Basics/38_Filtering_State/monsters-rolodex/src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+import { CardList } from './components/card-list/card-list.component';
+
+import './App.css';
+
+class App extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			monsters: [],
+			searchField: ''
+		};
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response => response.json())
+			.then(users => this.setState({ monsters: users }));
+	}
+
+
+	render() {
+		const { monsters, searchField } = this.state;
+		const filteredMonsters = monsters.filter(monster =>
+			monster.name.toLowerCase().includes(searchField.toLowerCase())
+		);
+
+		return (
+			<div className='App'>
+				<input
+					type='search'
+					placeholder='Search monster'
+					onChange={event => {
+						this.setState({ searchField: event.target.value }, () => {
+							console.log(this.state);
+						});
+					}} />
+				<CardList monsters={filteredMonsters} />
+			</div >
+		);
+	}
+}
+
+export default App;
